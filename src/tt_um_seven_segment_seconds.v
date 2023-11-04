b80092226f9d4840cae7b4ea4f29d38ad0adbbb9
davidparent <david.parent@sjsu.edu>
2023-11-03 17:11:54 -0700

    Update tt_um_qifv

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -10,51 +10,26 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+    reg signed [7:0] I_syn, // Synaptic current input (8-bit signed)
+    reg signed [7:0] V_mem, // Membrane potential (8-bit signed)
     wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+    assign uio_out = V_mem;
+    assign uio_in = I_syn;
+ 
 
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
+);
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
+  parameter signed [7:0] V_reset = -20; // Reset potential
+  parameter signed [7:0] V_th = 50;   // Threshold potential (Adjust as needed)
 
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
+      V_mem <= V_reset;
+    end else if (V_mem >= V_th) begin
+      V_mem <= V_reset;
+    end else begin
+        V_mem <= V_mem +  (V_mem/8)*(V_mem/8) ui_in/4; // Integrate the synaptic current
     end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+  end
+ 
 endmodule
