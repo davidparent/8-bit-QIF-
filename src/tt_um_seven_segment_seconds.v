53b46856b6f17cea35a781011d452b22d4a36654
davidparent <david.parent@sjsu.edu>
2023-11-03 17:19:48 -0700

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_davidparent_qif #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
