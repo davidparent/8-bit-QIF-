125d6e6145e875d755958e16221ef6c170a40905
davidparent <david.parent@sjsu.edu>
2023-11-14 10:09:56 -0800

    Update tt_um_QIF_8bit.v

--- src/tt_um_QIF_8bit.v
+++ src/tt_um_QIF_8bit.v
@@ -2,7 +2,7 @@
 
 module tt_um_QIF_8bit #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input vector b
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the ouptut of membrane
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the output of membrane
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path Not used
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path Not used
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
