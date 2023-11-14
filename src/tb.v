fa6c535a8fce10cad37834439b38f2c298747e4d
davidparent <david.parent@sjsu.edu>
2023-11-14 10:12:39 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_QIF_8bit tt_um_QIF_8bit (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
