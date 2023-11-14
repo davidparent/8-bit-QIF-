8992596830b4cf3dae8ae04c90d2484bb7bcd601
davidparent <david.parent@sjsu.edu>
2023-11-14 10:24:22 -0800

    Update tt_um_QIF_8bit.v

--- src/tt_um_QIF_8bit.v
+++ src/tt_um_QIF_8bit.v
@@ -11,14 +11,20 @@ module tt_um_QIF_8bit #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
     assign uio_oe = 8'b00000001;
-    //assign uio_out[7:1] = 7'd0;
+    assign uio_out[7:0] = 8'd0;
    
      QIFNeuron neuron (
        .clk(clk),
        .rst_n(rst_n),
        .I_syn(ui_in),
-       .V_mem(uo_out)
-       )
+       .V_mem(uo_out))
+     );
+    QIFNeuron neuron (
+       .clk(clk),
+       .rst_n(rst_n),
+       .B(ui_in),
+       .V(uo_out),
+       .spike_out(uio_out[0])
      );
 
 endmodule
