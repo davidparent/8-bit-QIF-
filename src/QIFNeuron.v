9c8b6e04b204e522e043ca6d912c722d1f2d6297
davidparent <david.parent@sjsu.edu>
2023-11-14 10:09:09 -0800

    Create QIFNeuron.v

---
+++ src/QIFNeuron.v
@@ -0,0 +1,25 @@
+module QIFNeuron (
+  input wire clk,          // Clock input
+  input wire rst_n,       // Reset input
+  input  signed reg [7:0] I_syn,     // Input B (8-bit, signed)
+  output signed reg [7:0] V_mem,     // Output voltage V (8-bit, signed)
+  
+);
+
+  parameter signed [7:0] V_reset = -20; // Reset potential
+  parameter signed [7:0] V_th = 50;   // Threshold potential (Adjust as needed)
+  always @(posedge clk or posedge reset) begin
+        if (rst_n) begin
+           V_mem <= V_reset;
+        end else if (V_mem >= V_th) begin
+           V_mem <= V_reset;
+    end else begin
+        V_mem <= V_mem +  (V_mem/8)*(V_mem/8) I_syn/4; // Integrate the synaptic current
+    end
+  end  
+
+  // Assign spike_out_reg to spike_out (output wire)
+  assign spike_out = spike_out_reg;
+  assign V = Z2;
+
+endmodule
