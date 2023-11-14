f23c0288f25ebba4cc28753e5a6cfc668afa4429
davidparent <david.parent@sjsu.edu>
2023-11-14 12:08:41 -0800

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -8,13 +8,9 @@ module QIFNeuron (
   wire  [7:0] V_reset =  -8'sd20; // Reset potential
   wire  [7:0] V_th = 8'd50;   // Threshold potential (Adjust as needed)
   always @(posedge clk or posedge rst_n) begin
-        if (rst_n) begin
+        if (rst_n== 1'b1) 
            V_mem <= V_reset;
-        end else if (V_mem >= V_th) begin
-           V_mem <= V_reset;
-    end else begin
-      V_mem <= V_mem +  (V_mem/8)*(V_mem/8)+ I_syn/4; // Integrate the synaptic current
-    end
-  end  
-
+        else
+            V_mem <= V_th;
+  end
 endmodule
