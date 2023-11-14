4708f97d4c172a6a9db1d2185169a66e1fc573fe
davidparent <david.parent@sjsu.edu>
2023-11-14 11:33:21 -0800

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -1,13 +1,13 @@
 module QIFNeuron (
   input wire clk,          // Clock input
   input wire rst_n,       // Reset input
-  input  signed reg [7:0] I_syn,     // Input B (8-bit, signed)
+  input  signed wire [7:0] I_syn,     // Input B (8-bit, signed)
   output signed reg [7:0] V_mem     // Output voltage V (8-bit, signed)
   
 );
 
-  reg signed [7:0] V_reset =  -8'sd20; // Reset potential
-  reg signed [7:0] V_th = 8'd50;   // Threshold potential (Adjust as needed)
+  parameter signed [7:0] V_reset =  -8'sd20; // Reset potential
+  parameter signed [7:0] V_th = 8'd50;   // Threshold potential (Adjust as needed)
   always @(posedge clk or posedge rst_n) begin
         if (rst_n) begin
            V_mem <= V_reset;
