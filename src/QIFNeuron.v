f964375a5d155898db5efecf8e29cf8cc9b4b252
davidparent <david.parent@sjsu.edu>
2023-11-14 15:05:10 -0800

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -4,8 +4,8 @@ module QIFNeuron (
   input   wire [7:0] I_syn,     // Input B (8-bit, signed)
   output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
-  reg [7:0] V
-  reg [7:0] B
+  reg [7:0] V;
+  reg [7:0] B;
   wire reset = ! rst_n;
    //reg [7:0] Z1, Z2;        // Delay flip-flops
   //wire [7:0] V_reset = -8'sd20; // Set Vreset to -20
