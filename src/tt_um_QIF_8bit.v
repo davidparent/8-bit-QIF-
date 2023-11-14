455333cb990f67ddb885e3fac5a3718a2269d979
davidparent <david.parent@sjsu.edu>
2023-11-14 10:00:38 -0800

    Create tt_um_QIF_8bit.v

---
+++ src/tt_um_QIF_8bit.v
@@ -0,0 +1,24 @@
+`default_nettype none
+
+module tt_um_QIF_8bit #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input vector b
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the ouptut of membrane
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path Not used
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path Not used
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+    assign uio_oe = 8'b00000001;
+    //assign uio_out[7:1] = 7'd0;
+   
+     QIFNeuron neuron (
+       .clk(clk),
+       .rst_n(rst_n),
+       .B(ui_in),
+       .V(uo_out),
+       .spike_out(uio_out[0])
+     );
+
+endmodule
